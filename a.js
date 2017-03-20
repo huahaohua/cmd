c5d2c7d812c762cd5918b81e3d371cdfe11d9ea9
USER-BPAES1L1VC\Administrator <1169756883@qq.com>
2017-03-20 10:11:40 +0800

    哈哈哈

--- a.js
+++ a.js
@@ -11,7 +11,8 @@
                         return $scope.items;
                     }
                 }
-            })
+            });
+			var a, b = 0;
             x.x.x.x.x //在这可以把父controller的变量进行传递，以便modal的controller使用
 
             eg: modalInstance.orderId = $scope.orderId;
